56d4e457716ec2763726a1fa9b1d51dda76200f4
ReasonJ01 <ReasonJ01@pudseygrangefield.co.uk>
2019-12-09 16:30:02 +0000

    v0.4
    Added db insert

--- app.py
+++ app.py
@@ -6,14 +6,33 @@ import json
 
 app = Flask(__name__)
 app.secret_key = "secret key"
-app.config["MONGO_URI"] = "mongodb://localhost:27017/movies"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/comics"
 mongo = PyMongo(app)
 
-
-@app.route('/movie', methods=['POST','GET'])
+@app.route('/movie', methods=['POST'])
 def movie():
     comic_id = request.form['comic_id']
-    r = requests.get("http://xkcd.com/"+comic_id+"/info.0.json")
+    r = requests.get("http://xkcd.com/" + comic_id + "/info.0.json")
+    json_object = r.json()
+
+    title = json_object['title']
+    img = json_object['img']
+    alt = json_object['alt']
+    day = json_object['day']
+    month = json_object['month']
+    year = json_object['year']
+    num = json_object['num']
+    date = day+'/'+month+'/'+year
+
+    #return a
+    #return str(items)
+    return render_template('movie.html', num=num, title=title, img=img, date=date, alt=alt)
+
+@app.route('/save',defaults={'num': '1'})
+@app.route('/save/<num>', methods=['POST','GET'])
+def save(num):
+    comic_id = num
+    r = requests.get("http://xkcd.com/" + comic_id + "/info.0.json")
     json_object = r.json()
 
     title = json_object['title']
@@ -26,68 +45,46 @@ def movie():
     date = day+'/'+month+'/'+year
 
 
-
-    #return a
-    #return str(items)
-    return render_template('movie.html', num=num, title=title, img=img, date=date, alt=alt)
-
-@app.route('/save')
-@app.route('/save/<num>', methods=['POST','GET'])
-def info(num):
-    comic_id = num
-    r = requests.get("http://xkcd.com/"+comic_id+"/info.0.json")
-    json_object = r.json()
-
-    title = json_object['title']
-    img = json_object['img']
-    alt = json_object['alt']
-    day = json_object['day']
-    month = json_object['month']
-    year = json_object['year']
-    num = json_object['num']
-
     if request.method == 'POST':
-        fav = mongo.db.savedComics.insert({'num' : num, 'title': title, 'img' : img, 'day' : day, 'month' : month, 'year' : year})
-        resp = 'Comic added to saved list'
+        fav = mongo.db.userComics.insert({'num': num, 'title': title, 'img': img, 'alt': alt, 'day': day, 'month': month, 'year': year, 'date': date})
+        resp = 'Added to Favourites'
         return resp
 
     #return json_object
-    return render_template('info.html', num=num, title=title, img=img, day=day, month=month, year=year, alt=alt)
+    return render_template('info.html', num=num, title=title, img=img, alt=alt, date=date)
 
 
-@app.route('/delete/<id>', methods=['POST'])
-def delete_movie(id):
-    mongo.db.userMovies.delete_one({'_id': id})
-    resp = 'Movie removed successfully!'
-    return resp
+@app.route('/delete/<num>', methods=['POST'])
+def delete_movie(num):
+    mongo.db.userComics.delete_one({'num': num})
+    resp = 'Comic removed successfully!'
+    return userFavs()
 
-@app.route('/userFavs',)
+@app.route('/userComics',)
 def userFavs():
-    favMovies = mongo.db.userMovies.find()
-    return render_template('userMovies.html', favMovies=favMovies)
+    favComics = mongo.db.userComics.find()
+    return render_template('userMovies.html', favComics=favComics)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/infosearch')
-def infosearch():
-	return render_template('info-search.html')
 
-
-@app.route('/watched/<id>', methods=['POST'])
-def watched_movie(id):
-    mongo.db.userMovies.update({'_id': id}, {"$set": {"watched": "true"}})
-    mongo.db.userMovies.find().sort("watched", 1)
-    resp = 'Movie set to watched successfully!'
+@app.route('/watched/<num>', methods=['POST'])
+def watched_movie(num):
+    mongo.db.userComics.update({'num': num}, {"$set": {"read": "true"}})
+    mongo.db.userComics.find().sort("watched", 1)
+    resp = 'Comic set to read successfully!'
     return userFavs()
 
-@app.route('/unwatched/<id>', methods=['POST'])
-def unwatched_movie(id):
-    mongo.db.userMovies.update({'_id': id}, {"$set": {"watched": "false"}})
-    mongo.db.userMovies.find().sort("watched", 1)
-    resp = 'Movie set to watched successfully!'
+#Not Communicating with db
+
+@app.route('/unwatched/<num>', methods=['POST'])
+def unwatched_movie(num):
+    mongo.db.userComics.update({'num': num}, {"$set": {"watched": "false"}})
+    mongo.db.userComics.find().sort("watched", 1)
+    resp = 'Comic set to unread successfully!'
     return userFavs()
 
 
